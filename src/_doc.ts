4c5581fbc7bb6b8581c04a3d85bd53f80a29b599
Northword <northword@outlook.com>
2024-09-24 21:58:18 +0800

    fix: update docs config

--- src/_doc.ts
+++ src/_doc.ts
@@ -2,40 +2,24 @@
  * This file is for api-extractor. Things here will appear in the docs.
  */
 
-import { BasicTool } from "./basic.js";
-import { ZoteroToolkit } from "./index.js";
-import { UITool, ElementProps } from "./tools/ui.js";
-import { ReaderTool } from "./tools/reader.js";
-import { ExtraFieldTool } from "./tools/extraField.js";
-import { FieldHookManager } from "./managers/fieldHook.js";
-import { KeyboardManager, KeyModifier } from "./managers/keyboard.js";
-import { MenuManager } from "./managers/menu.js";
-import { ClipboardHelper } from "./helpers/clipboard.js";
-import { FilePickerHelper } from "./helpers/filePicker.js";
-import { ProgressWindowHelper } from "./helpers/progressWindow.js";
-import { VirtualizedTableHelper } from "./helpers/virtualizedTable.js";
-import { DialogHelper } from "./helpers/dialog.js";
-import { LargePrefHelper } from "./helpers/largePref.js";
-import { PatchHelper } from "./helpers/patch.js";
-import { GuideHelper } from "./helpers/guide.js";
-
 export {
-  BasicTool,
   ZoteroToolkit,
+  BasicTool,
   UITool,
   ElementProps,
   ReaderTool,
   ExtraFieldTool,
-  FieldHookManager,
-  KeyboardManager,
-  KeyModifier,
+  // PromptManager,
   MenuManager,
-  PatchHelper,
   ClipboardHelper,
   FilePickerHelper,
   ProgressWindowHelper,
   VirtualizedTableHelper,
   DialogHelper,
+  FieldHookManager,
   LargePrefHelper,
+  KeyboardManager,
+  KeyModifier,
+  PatchHelper,
   GuideHelper,
-};
+} from "./index.js";
